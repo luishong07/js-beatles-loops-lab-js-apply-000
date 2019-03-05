5288a4643943febd5cadec50937c8fbf3a4bea33
algebraic-propagation-5468 <luishong50@gmail.com>
2019-03-05 21:39:39 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 // add solution here
 function theBeatlesPlay (musicians, instruments){
-  var empty =[]
-  var musicians =['John','Paul','George','Ringo']
-  var instruments = ['guitar','bass guitar','sitar','drums']
+  var empty =[];
+  var musicians =['John','Paul','George','Ringo'];
+  var instruments = ['guitar','bass guitar','sitar','drums'];
   for (var i=0;i<4;i++)
   return 
 }
